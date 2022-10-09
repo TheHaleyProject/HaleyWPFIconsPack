31503e6b8550229a0a97817cdab7f6915f3cf721
Senguttuvan Mahalingam <rmsmech@gmail.com>
2022-10-09 17:22:07 +0400

    Resource Pilot

---
+++ HaleyWPFResources/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Windows;
+using System.Windows.Markup;
+
+//[assembly: Guid("FECEB4DE-F5CA-4CB0-A18A-C48FAF964F38")]
+//[assembly: NeutralResourcesLanguage("en-GB")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
+
+[assembly: XmlnsPrefix("http://schemas.hpod9.com/haley/wpf", "hlyWPF")]
+
+//FOR XAML NAMESPACES - WPF
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/wpf", "Haley.WPF.Controls")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/wpf", "Haley.WPF")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/wpf", "Haley.Enums")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/wpf", "Haley.Events")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/wpf", "Haley.Models")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/wpf", "Haley.Utils")]
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# HaleyWPFResources
-Resources for the WPF Projects
+Okay
+# HaleyWPF
+Currently contains 14 User controls.
+
+1. Image button 
+2. Image Combo button
+3. Plain button
+4. Plain textbox 
+5. Plain Passwordbox
+6. Expander
+7. Separator
+8. Pagination
+9. Collection Selector
+10. Plain Listview
+11. Toggle Button
+12. Linked Text
+13. Plain Image
+14. Plain CheckBox
